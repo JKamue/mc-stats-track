8d798d3ff32c142f7f407241914e41f6f637a02a
JKamue <mail@jkamue.de>
2020-01-09 19:48:03 +0100

    Not adding new Dataset when one is present for the hour

--- classes/StatsCollector.class.php
+++ classes/StatsCollector.class.php
@@ -12,13 +12,23 @@ class StatsCollector
 
     public function saveHourlyStats(string $serverId)
     {
+        $time = (new DateTime())->format('Y-m-d H:00:00');
+
+        $alreadySaved = $this->db->query("SELECT '' FROM `data` WHERE `server` = ? AND `when` = ?")
+            ->bind(1, $serverId)
+            ->bind(2, $time)
+            ->count();
+
+        if ($alreadySaved > 0) {
+            return;
+        }
+
         $serverAdress = $this->db->query("SELECT adress FROM servers WHERE server = ?")
             ->bind(1, $serverId)
             ->single()["adress"];
 
         $minecraftPingData = \xPaw\MinecraftPing::getPingData($serverAdress);
 
-        $time = (new DateTime())->format('Y-m-d H:00:00');
         $this->db->query("INSERT INTO `data` (`when`, `online`, `max`, `server`) VALUES (?, ?, ?, ?)")
             ->bind(1, $time)
             ->bind(2, $minecraftPingData["players"]["online"])
